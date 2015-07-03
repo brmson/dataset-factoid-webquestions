f2529af9c6b9aea9df5b75eafb5bbda1cb9067d5
Petr Baudis <pasky@ucw.cz>
2015-07-04 01:11:37 +0200

    scripts/freebase_relpaths.py: Query in parallel using multiprocessing pool

--- scripts/freebase_relpaths.py
+++ scripts/freebase_relpaths.py
@@ -12,59 +12,67 @@
 
 from __future__ import print_function
 
+from multiprocessing import Pool
 from SPARQLWrapper import SPARQLWrapper, JSON
 import sys
 import datalib
 
 
-def get_mid(key, sparql):
-    sparql_mid_query = '''
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+class QuestionRelPathFinder:
+    """ a callable that takes care of producing a relpath dataset record
+    for a question """
+    def __init__(self, sparql, N):
+        self.sparql = sparql
+        self.N = N
 
-            SELECT ?concept WHERE {
-                ?concept <http://rdf.freebase.com/key/en> "''' + key + '''" .
-            }
-        '''
-    sparql.setQuery(sparql_mid_query)
-    results = sparql.query().convert()
-    return [r['concept']['value'] for r in results['results']['bindings']][0]
+    def get_mid(self, key):
+        sparql_mid_query = '''
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-
-def concept_rels_match(mid, labels, sparql):
-    """ list all relations of a given concept whose label or string value
-    is present in the given set of labels """
-
-    value_filter = ['STR(LCASE(?value)) = "' + l.lower() + '"' for l in labels]
-
-    sparql_rel_query = '''
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-            SELECT ?rel WHERE {
-                <''' + mid + '''> ?rel ?val .
-
-                OPTIONAL {
-                  ?val rdfs:label ?vallabel .
-                  FILTER(LANGMATCHES(LANG(?vallabel), \"en\"))
+                SELECT ?concept WHERE {
+                    ?concept <http://rdf.freebase.com/key/en> "''' + key + '''" .
                 }
-                BIND(IF(BOUND(?vallabel), ?vallabel, ?val) AS ?value)
-
-                FILTER(''' + ' || '.join(value_filter) + ''')
-            }
-        '''
-    # print(sparql_rel_query)
-    sparql.setQuery(sparql_rel_query)
-    results = sparql.query().convert()
-    return [[r['rel']['value']] for r in results['results']['bindings']]
+            '''
+        self.sparql.setQuery(sparql_mid_query)
+        results = self.sparql.query().convert()
+        return [r['concept']['value'] for r in results['results']['bindings']][0]
 
 
-def question_relpaths(q, sparql, N):
-    mid = get_mid(q['freebaseKey'], sparql)
-    print(q['freebaseKey'], mid, file=sys.stderr)
-    relpaths = concept_rels_match(mid, set(q['answers']), sparql)
-    print('\t', relpaths, file=sys.stderr)
+    def concept_rels_match(self, mid, labels):
+        """ list all relations of a given concept whose label or string value
+        is present in the given set of labels """
 
-    return {'qId': q['qId'],
-            'relPaths': relpaths}
+        value_filter = ['STR(LCASE(?value)) = "' + l.lower() + '"' for l in labels]
+
+        sparql_rel_query = '''
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                SELECT ?rel WHERE {
+                    <''' + mid + '''> ?rel ?val .
+
+                    OPTIONAL {
+                      ?val rdfs:label ?vallabel .
+                      FILTER(LANGMATCHES(LANG(?vallabel), \"en\"))
+                    }
+                    BIND(IF(BOUND(?vallabel), ?vallabel, ?val) AS ?value)
+
+                    FILTER(''' + ' || '.join(value_filter) + ''')
+                }
+            '''
+        # print(sparql_rel_query)
+        self.sparql.setQuery(sparql_rel_query)
+        results = self.sparql.query().convert()
+        return [[r['rel']['value']] for r in results['results']['bindings']]
+
+
+    def __call__(self, q):
+        mid = self.get_mid(q['freebaseKey'])
+        print(q['freebaseKey'], mid, file=sys.stderr)
+        relpaths = self.concept_rels_match(mid, set(q['answers']))
+        print(q['freebaseKey'], '    ', relpaths, file=sys.stderr)
+
+        return {'qId': q['qId'],
+                'relPaths': relpaths}
 
 
 if __name__ == "__main__":
@@ -75,5 +83,15 @@ if __name__ == "__main__":
     sparql = SPARQLWrapper(endpoint)
     sparql.setReturnFormat(JSON)
 
+    # XXX: We would like to write the JSON file as we go, but we need
+    # to test for last element in save_json() and things would just
+    # get too complicated too needlessly.
+
+    qrpf = QuestionRelPathFinder(sparql, N)
+    pool = Pool()
+    qrp = pool.imap(qrpf, data.to_list())
+    pool.close()
+    pool.join()
+
     with open('d-freebase-rp/%s.json' % (split,), 'w') as f:
-        datalib.save_json([question_relpaths(q, sparql, N) for q in data.to_list()], f)
+        datalib.save_json(qrp, f)
