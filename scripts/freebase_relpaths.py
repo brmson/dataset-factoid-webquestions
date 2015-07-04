8a18f2a73f1fd2a91f0d3517a806ff308ad2433e
Petr Baudis <pasky@ucw.cz>
2015-07-04 10:42:18 +0200

    scripts/freebase_relpaths.py: Add support for N=2
    The problem is that an N=2 query for wqr000000 did not finish after
    8 hours. We'll redo it using the Google API, even though it's
    deprecated.

--- scripts/freebase_relpaths.py
+++ scripts/freebase_relpaths.py
@@ -5,9 +5,12 @@
 # Walk the RDF graph with Freebase data up to degree N trying to match
 # one of the answers, storing all relations that do lead to one.
 #
+# N.B. to save time, if we find matching relations for N=1, we do not search N=2
+# anymore.
+#
 # Example:
 #   mkdir d-freebase-rp
-#   N=1
+#   N=2
 #   for split in devtest val trainmodel test; do echo $split; ./freebase_relpaths.py $split http://freebase.ailao.eu:3030/freebase/query $N; done
 
 from __future__ import print_function
@@ -83,14 +86,39 @@ class QuestionRelPathFinder:
         results = self.sparql.query().convert()
         return self.count_paths([[r['rel']['value']] for r in results['results']['bindings']])
 
+    def concept_rels2_match(self, mid, labels):
+        """ list all relations of a given concept whose label or string value
+        is present in the given set of labels, while traversing some other
+        node """
+        sparql_rel_query = '''
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                SELECT ?rel0 ?rel1 WHERE {
+                    <''' + mid + '''> ?rel0 ?t .
+                    FILTER(ISURI(?t))
+                    ?t ?rel1 ?val .
+                    ''' + self.sparql_filter(labels) + '''
+                }
+            '''
+        # print(sparql_rel_query)
+        self.sparql.setQuery(sparql_rel_query)
+        results = self.sparql.query().convert()
+        return self.count_paths([[r['rel0']['value'], r['rel1']['value']] for r in results['results']['bindings']])
+
     def __call__(self, q):
         try:
             mid = self.get_mid(q['freebaseKey'])
             print(q['freebaseKey'], mid, file=sys.stderr)
             relpaths = self.concept_rels_match(mid, set(q['answers']))
+            if self.N >= 2 and not relpaths:
+                print(q['freebaseKey'], '    ', 'N=2', file=sys.stderr)
+                relpaths = self.concept_rels2_match(mid, set(q['answers']))
+            if self.N > 2:
+                raise Exception('only N<=2 is supported')
             print(q['freebaseKey'], '    ', relpaths, file=sys.stderr)
         except:
             traceback.print_exc()
+            raise
 
         return {'qId': q['qId'],
                 'relPaths': relpaths}
@@ -108,9 +136,9 @@ if __name__ == "__main__":
     # to test for last element in save_json() and things would just
     # get too complicated too needlessly.
 
-    qrpf = QuestionRelPathFinder(sparql, N)
-    pool = Pool()
-    qrp = pool.imap(qrpf, data.to_list())
+    qrpf = QuestionRelPathFinder(sparql, int(N))
+    pool = Pool(processes=1)
+    qrp = pool.map(qrpf, data.to_list())
     pool.close()
     pool.join()
 
