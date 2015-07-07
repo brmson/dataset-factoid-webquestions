f37ea2ec1319fc7448f8a9a8ebdc462ebbd0f510
Petr Baudis <pasky@ucw.cz>
2015-07-07 18:40:09 +0200

    scripts/json2tsv.py: Take input dir as argument too
    Useful when running on topic subsets like t-movies/.

--- scripts/json2tsv.py
+++ scripts/json2tsv.py
@@ -3,7 +3,7 @@
 # json2tsv.py: Create YodaQA-compatible TSV file from the main JSON file
 #
 # Example (after running mktrain):
-#   mkdir tsv; for s in train test; do ./json2tsv.py $s tsv/; done
+#   mkdir tsv; for s in train test; do ./json2tsv.py main $s tsv/; done
 
 from __future__ import print_function
 
@@ -13,9 +13,9 @@ import sys
 
 
 if __name__ == "__main__":
-    split, outdirname = sys.argv[1:]
+    indir, split, outdirname = sys.argv[1:]
 
-    with open('main/%s.json' % (split,), 'r') as f:
+    with open('%s/%s.json' % (indir, split), 'r') as f:
         data = json.load(f)
 
     with io.open('%s/%s.tsv' % (outdirname, split), 'w', encoding='utf8') as f:
