09e3da6c3f06f0cba2f968d97a0553c3443f8dc8
Jan Pichl <honza.pichl@gmail.com>
2016-04-28 10:50:30 +0200

    Escaped csv dataset, added prefixes to files which have entities replaced

--- scripts/remove-multilabel-pairs.py
+++ scripts/remove-multilabel-pairs.py
@@ -16,10 +16,13 @@ with open(file_name) as f:
             else:
                 pos[key] = 1
             
-
+outcsv = csv.DictWriter(sys.stdout, fieldnames=['qtext', 'label', 'atext'])
+outcsv.writeheader()
 with open(file_name) as f:
     r = csv.reader(f, delimiter=',')
     for s in r:
+        if (s[0] == 'qtext'):
+            continue
         key = s[0] + ',' + s[2] 
         if (key not in pos or s[1] == '1'):
-            print(','.join(s))
+            outcsv.writerow({'qtext':s[0], 'label':s[1], 'atext':s[2]})
